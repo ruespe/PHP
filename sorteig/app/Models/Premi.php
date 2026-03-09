f958b515b9152cb4c0e7a38e3be479b2419f8f2f
ruespe <rubenespin84@gmail.com>
2026-03-09 15:14:21 +0100

    a

--- sorteig/app/Models/Premi.php
+++ sorteig/app/Models/Premi.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Premi extends Model
 {
-    protected $table = 'premis';
-
 
     protected $fillable = [
         'nom',
--- sorteig/database/migrations/2026_02_19_173943_create_premis_table.php
+++ sorteig/database/migrations/2026_02_19_173943_create_premis_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nom');
             $table->string('valor');
+            $table->enum('premis'[ premis: 'ratolí', 'cotxe', 'viatge', 'dinar', 'massatge']);
             $table->timestamps();
         });
     }
