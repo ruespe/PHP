766edfa396a92e887f5cce7cecf022b19b9030ff
ruespe <rubenespin84@gmail.com>
2026-03-23 20:47:10 +0100

    cookiues

--- RECUra1PHP/cook2.php
+++ RECUra1PHP/cook2.php
@@ -4,20 +4,49 @@ A més afegeix dos enllaços, un per actualitzar la pàgina en quant a data, hor
 
 <?php
 
-use Illuminate\Support\Facades\Date;
 
-    // $fechaActual = Date();
+$fechaActual = new DateTime();
+
+setcookie("accesoFecha", $fechaActual->format('d-m-Y H:m:s'), time() + 3600);
+
+if(isset($_COOKIE["accesoContador"])){
+    $contador = $_COOKIE["contador"] + 1;
+    
+}
+
+setcookie("accesoContador", $contador, time() + 3600);
+
+if (isset($_POST['actualizar'])) {
+    $ultimo_acceso = $_COOKIE["accesoFecha"];
+}
+
+if (isset($_POST['borrar'])) {
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
+
+    <?php
+    echo "Data i hora actual: " .  $fechaActual->format('d-m-Y H:m:s');
+
+    echo "<br>";
+
+    echo "L'ultima vegada que vas accedir a aquesta pàgina va ser: " .
+    $ultimo_acceso;
+
+    echo "Quantitat d'accesos a aquesta pàgina: " . $contador;
+    ?>
 </body>
+
 </html>
