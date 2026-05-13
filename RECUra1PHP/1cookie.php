366b67a58f854aa38fca9579bab4fe7a308c06c6
ruespe <rubenespin84@gmail.com>
2026-05-13 13:20:18 +0200

    a

--- RECUra1PHP/1cookie.php
+++ RECUra1PHP/1cookie.php
@@ -6,21 +6,46 @@ A cada connexió hauràs de comprovar si existeix la galeta corresponent per sal
 
 A més, afegeix un camp que permeti a l’usuari comunicar que vol esborrar la cookie. En aquest cas s'haurà d’eliminar. -->
 
+
+<?php
+
+if (isset($_GET['eliminar'])) {
+    setcookie('nom', '', time() - 3600);
+    unset($_COOKIE['nom']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+
+if (isset($_GET['nom']) && !empty($_GET['nom'])) {
+    $nom = htmlspecialchars(trim($_GET['nom']));
+    setcookie('nom', $nom, time() + 3600);
+    $_COOKIE['nom'] = $nom;
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <input type="text" name="cookie">
-    <button type="submit">Submit</button>
-    <p><a href="">Eliminar cookie</a></p>
-    <?php
-        echo "Benvingut $nom";
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="GET">
+        <input type="text" name="nom">
+        <button type="submit">Submit</button>
 
-    ?>
-
+        <?php
+        if (isset($_COOKIE['nom']) && !empty($_COOKIE['nom'])) {
+            echo "Benvingut " . $_COOKIE['nom'];
+        }
+        ?>
+        <button name="eliminar">Eliminar</button>
+    </form>
 </body>
+
 </html>
