e3fa90dd6ef148cbb82f089da3fa1aa06646bd3b
ruespe <rubenespin84@gmail.com>
2026-05-13 13:45:15 +0200

    a

--- ra3Estudiar/todo/app/Models/Tasca.php
+++ ra3Estudiar/todo/app/Models/Tasca.php
@@ -2,14 +2,21 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tasca extends Model
 {
+    protected $table = 'tasques';
     protected $fillable = [
         'titol',
         'descripcio',
         'prioritat',
         'estat',
+        'categoria_id'
     ];
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
 }
